53f61abda747c06addd96ca098b6e4ce172a119b
fluffysoo <752407060@qq.com>
2023-10-09 23:29:15 +0800

    it is my second change

--- exp3.cpp
+++ exp3.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     cout << "This is initial." << endl;
-    cout << "This is first change." << endl;
+    cout << "This is second change." << endl;
     return 0;
 }
